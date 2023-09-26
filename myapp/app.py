fca9d787f32dbc0dadc5ef1b6a50324454ccb5c0
julefikar <80801138+julefikar@users.noreply.github.com>
2023-09-26 15:29:27 -0400

    linking database

--- myapp/app.py
+++ myapp/app.py
@@ -1,10 +1,25 @@
+import certifi
 from flask import Flask
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 
+app.config.from_pyfile('../dev.env')
+
+def get_mongo_client():
+    uri = app.config['MONGODB_URI']
+    client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+    return client
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    client = get_mongo_client()
+    try:
+        client.admin.command('ping')
+        return "Pinged your deployment. You successfully connected to MongoDB!"
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run()
