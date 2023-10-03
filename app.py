3d5c393dfeb96bf6da8e930bbbd147522a28bc90
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2023-10-02 22:03:41 -0400

    Set up post requests, now need to make GET requests based on job id

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-from flask import Flask
+import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/get_price_data', methods = ['GET', 'POST'])
+def get_price_data():
+    token = 'OCNXKSNMBFLRRLWZWANKMIOLWSVWEAUYBCHWCADJLMYTVBAVKKNJGPFNZLUDXTVG'
+    data = {
+        'token': token,
+        'country': 'us',
+        'source': 'google_shopping',
+        'topic': 'product_and_offers',
+        'key': 'term',
+        'max_age': '1200',
+        'max_pages': '1',
+        'sort_by': 'ranking_descending',
+        'condition': 'any',
+        'shipping': 'any',
+        'values': 'Iphone 12 blue'
+    }
 
-if __name__ == '__main__':
-    app.run()
+    response = requests.post('https://api.priceapi.com/v2/jobs', data=data)
+    
+    #GET REQUEST IN FOLLOWING FORMAT
+    #https://api.priceapi.com/v2/jobs/1234567890abcdef12345678?token=$PRICEAPI_TOKEN
 
-'''
-    curl "https://api.priceapi.com/v2/jobs" 
->         -X POST \
->         -d "token=OCNXKSNMBFLRRLWZWANKMIOLWSVWEAUYBCHWCADJLMYTVBAVKKNJGPFNZLUDXTVG" \
->         -d "country=us" \
->         -d "source=google_shopping" \
->         -d "topic=product_and_offers" \
->         -d "key=term" \
->         -d "max_age=1200" \
->         -d "max_pages=1" \
->         -d "sort_by=ranking_descending" \
->         -d "condition=any" \
->         -d "shipping=any" \
->         -d 'values=light blue jeans' 
+    if response.status_code == 200:
+        return jsonify(response.json())
+    return jsonify({'error': 'Failed to retrieve data from external API'}), response.status_code
 
-'''
+    if __name__ == '__main__':
+        app.run(debug=True)
