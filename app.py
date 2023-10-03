61cc229735a2a9450a7fba9e3a035840027e34d2
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2023-10-02 20:54:22 -0400

    Curl format

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
+'''
+    curl "https://api.priceapi.com/v2/jobs" 
+>         -X POST \
+>         -d "token=OCNXKSNMBFLRRLWZWANKMIOLWSVWEAUYBCHWCADJLMYTVBAVKKNJGPFNZLUDXTVG" \
+>         -d "country=us" \
+>         -d "source=google_shopping" \
+>         -d "topic=product_and_offers" \
+>         -d "key=term" \
+>         -d "max_age=1200" \
+>         -d "max_pages=1" \
+>         -d "sort_by=ranking_descending" \
+>         -d "condition=any" \
+>         -d "shipping=any" \
+>         -d 'values=light blue jeans' 
+
+'''
