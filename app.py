5cc8e0398d93fb1f510f70e896f172bc0fe8ed49
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2023-10-18 12:45:07 -0400

    Final changes

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def get_price_data():
         'key': 'term',
         'max_age': '1200',
         'max_pages': '1',
-        'sort_by': 'price_ascending',
+       # 'sort_by': 'price_ascending',
         'condition': 'any',
         'shipping': 'any',
-        'values': 'Black Jeans'
+        'values': 'Argentina National Team Jersey'
     }
 
     post_response = requests.post('https://api.priceapi.com/v2/jobs', data=data)   
     job_id = post_response.json()['job_id']
 
     time_alloted, max_time = 0, 60
-    while True:
+    while time_alloted < max_time:
         response = requests.get(f'https://api.priceapi.com/v2/jobs/{job_id}/download?token={token}')
 
         if response.status_code == 200:
